b47670eb47cc2b310fd3bf62b951eb5f85bbbc63
Adenikemi <adenikeawolalu@gmail.com>
2025-06-16 07:50:23 +0100

    commit changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsModule, CommonModule, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './features/about/about.component';
+import { HomeComponent } from './features/home/home.component';
+import { ServicesComponent } from './features/services/services.component';
+import { ContactComponent } from './features/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   path: "",
+        component: HomeComponent
+    },
+    { path: "services", component: ServicesComponent },
+    { path: "about", component: AboutComponent },
+    { path: "contact", component: ContactComponent }
+];
