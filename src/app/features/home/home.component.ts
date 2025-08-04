66194644b06a0a52b2fb35e386b4b039f32383c9
Adenikemi <adenikeawolalu@gmail.com>
2025-08-04 06:36:15 +0100

    commit page changes

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,10 @@
-import { NgFor } from '@angular/common';
+// import { NgFor } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [NgFor],
+  // imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
